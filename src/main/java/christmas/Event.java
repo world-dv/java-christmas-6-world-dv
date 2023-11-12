bf55989432d8829843f530b87b12c34721cc14cc
HJLee <goodforx0147@naver.com>
2023-11-13 04:35:38 +0900

    feat: calculate giveaway event price - 25,000, remake print giveaway event

--- src/main/java/christmas/Event.java
+++ src/main/java/christmas/Event.java
@@ -2,30 +2,32 @@ package christmas;
 
 public class Event {
     private final Boolean applyEvent;
-    private final Boolean giveawayEvent;
+    private final Integer giveawayEvent;
     private final Integer christmasEvent;
     private final Integer specialEvent;
+    private final Integer sumOfEventPrice;
 
     public Event(OrderList orderList, DateToVisit dateToVisit) {
         this.applyEvent = isOrderApplyEvent(orderList);
-        this.giveawayEvent = isOrderApplyGiveawayEvent(orderList);
+        this.giveawayEvent = applyGiveawayEvent(orderList.getOrderPrice());
         this.christmasEvent = applyChristmasEvent(dateToVisit.getVisitDate());
         this.specialEvent = applySpecialEvent(dateToVisit.getStarDate());
+        this.sumOfEventPrice = sumEventPrice();
     }
 
     private Boolean isOrderApplyEvent(OrderList orderList) {
         return orderList.getOrderPrice() >= 10000;
     }
 
-    private Boolean isOrderApplyGiveawayEvent(OrderList orderList) {
-        return orderList.getOrderPrice() >= 120000;
+    private Boolean isOrderApplyGiveawayEvent(int orderPrice) {
+        return orderPrice >= 120000;
     }
-
-    private String applyGiveawayEvent() {
-        if (giveawayEvent && applyEvent) {
-            return "샴페인 1개";
+    
+    private Integer applyGiveawayEvent(int orderPrice) {
+        if (isOrderApplyGiveawayEvent(orderPrice) && applyEvent) {
+            return -25000;
         }
-        return "없음";
+        return 0;
     }
 
     private Boolean isOrderApplyChristmasEvent(int visitDate) {
@@ -46,7 +48,18 @@ public class Event {
         return 0;
     }
 
+    private Integer sumEventPrice() {
+        return christmasEvent + specialEvent + giveawayEvent;
+    }
+
+    private String getChampagne() {
+        if (giveawayEvent == 0) {
+            return "없음";
+        }
+        return "샴페인 1개";
+    }
+    
     public void printGiveawayEvent() {
-        System.out.println("<증정 메뉴>\n" + applyGiveawayEvent());
+        System.out.println("<증정 메뉴>\n" + getChampagne());
     }
 }
