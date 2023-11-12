ed713dbec87dc87a92fa7fd520988acf27f9efd5
HJLee <goodforx0147@naver.com>
2023-11-13 00:18:34 +0900

    feat: modify class to pulic class, add menu class

--- src/main/java/christmas/DateToVisit.java
+++ src/main/java/christmas/DateToVisit.java
@@ -8,7 +8,7 @@ public class DateToVisit {
     private final Boolean startDate;
     List<Integer> starDateList = Arrays.asList(3, 10, 17, 24, 25, 31);
 
-    DateToVisit(Integer date) {
+    public DateToVisit(Integer date) {
         validate(date);
         this.visitDate = date;
         this.startDate = isStarDate(date);
---
+++ src/main/java/christmas/Menu.java
@@ -0,0 +1,11 @@
+package christmas;
+
+public class Menu {
+    private final String name;
+    private final Integer price;
+
+    public Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
