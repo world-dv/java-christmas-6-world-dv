038a9bc380e3cec0f8a274c724eace754f5e36dd
HJLee <goodforx0147@naver.com>
2023-11-13 02:23:28 +0900

    feat: create order list to order - menu name + order count, calculate num order count

--- src/main/java/christmas/OrderList.java
+++ src/main/java/christmas/OrderList.java
@@ -5,15 +5,28 @@ import java.util.List;
 
 public class OrderList {
     private final List<Order> orderList;
+    private final Integer orderNum;
 
-    public OrderList(List<String> orders) {
+    public OrderList(List<String> order) {
+        List<Order> orders = createOrderList(order);
+        validate(orders);
+
+        this.orderList = orders;
+        this.orderNum = numOrder(orders);
+    }
+
+    private void validate(List<Order> orders) {
+        if (isCountOver20(orders)) {
+            throw new IllegalArgumentException("[ERROR] 주문 음식이 20개가 넘을 경우 주문이 불가합니다.");
+        }
+    }
+
+    private List<Order> createOrderList(List<String> orders) {
         List<Order> orderList = new ArrayList<>();
-
         for (String order : orders) {
             orderList.add(createOrder(splitOrder(order)));
         }
-
-        this.orderList = orderList;
+        return orderList;
     }
 
     private List<String> splitOrder(String order) {
@@ -22,6 +35,18 @@ public class OrderList {
 
     private Order createOrder(List<String> order) {
         return new Order(order.get(0), Integer.parseInt(order.get(1)));
+    }
+
+    private Integer numOrder(List<Order> orders) {
+        int numCount = 0;
+        for (Order order : orders) {
+            numCount += order.getCount();
+        }
+        return numCount;
+    }
+
+    private Boolean isCountOver20(List<Order> orders) {
+        return numOrder(orders) > 20;
     }
 
     public void printOrderList() {
