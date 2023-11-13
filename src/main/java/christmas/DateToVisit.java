cd4aa79801510ab96a20a26778f2a843ab74c0a3
HJLee <goodforx0147@naver.com>
2023-11-13 13:17:36 +0900

    feat: calculate day of visit date

--- src/main/java/christmas/DateToVisit.java
+++ src/main/java/christmas/DateToVisit.java
@@ -6,12 +6,15 @@ import java.util.List;
 public class DateToVisit {
     private final Integer visitDate;
     private final Boolean startDate;
+    private final String dayOfDate;
     List<Integer> starDateList = Arrays.asList(3, 10, 17, 24, 25, 31);
+    List<String> day = Arrays.asList("금", "토", "일", "월", "화", "수", "목");
 
     public DateToVisit(Integer date) {
         validate(date);
         this.visitDate = date;
         this.startDate = isStarDate(date);
+        this.dayOfDate = calculateDay(date);
     }
 
     private void validate(int date) {
@@ -28,11 +31,19 @@ public class DateToVisit {
         return starDateList.contains(date);
     }
 
+    private String calculateDay(int date) {
+        return day.get((date - 1) % 7);
+    }
+
     public Integer getVisitDate() {
         return visitDate;
     }
 
     public Boolean getStarDate() {
         return startDate;
+    }
+
+    public String getDayOfDate() {
+        return dayOfDate;
     }
 }
