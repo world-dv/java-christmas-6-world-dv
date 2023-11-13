4e996e1dcc3f1c3d7101dcb1d56bd5c34eb30d2b
HJLee <goodforx0147@naver.com>
2023-11-13 15:38:00 +0900

    refactor: separate input -> input view class

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -8,8 +8,9 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
-        DateToVisit dateToVisit = inputDate();
-        OrderList orderList = inputOrder();
+        InputView inputView = new InputView();
+        DateToVisit dateToVisit = inputView.inputDate();
+        OrderList orderList = inputView.inputOrder();
 
         System.out.println("12월 " + dateToVisit.getVisitDate() + "일에 우테코 식당에서 받을 이벤트 혜택 미리보기!\n");
         orderList.printOrderList();
@@ -17,33 +18,5 @@ public class Application {
 
         Event event = new Event(orderList, dateToVisit);
         event.printGiveawayEvent();
-    }
-
-    public static DateToVisit inputDate() {
-        while(true) {
-            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!)");
-            try {
-                int date = Integer.parseInt(Console.readLine());
-                return new DateToVisit(date);
-            } catch (NumberFormatException e) {
-                System.out.println("[ERROR] 식당 예상 방문 날짜는 숫자여야 합니다.");
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-    }
-
-    public static OrderList inputOrder() {
-        while(true) {
-            System.out.println("주문하실 메뉴를 메뉴와 개수를 알려주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-            try {
-                List<String> orders = List.of(Console.readLine().split(","));
-                return new OrderList(orders);
-            } catch (NumberFormatException e) {
-                System.out.println("[ERROR] 메뉴의 개수는 숫자여야 합니다.");
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
     }
 }
---
+++ src/main/java/christmas/InputView.java
@@ -0,0 +1,35 @@
+package christmas;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class InputView {
+    public DateToVisit inputDate() {
+        while(true) {
+            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!)");
+            try {
+                int date = Integer.parseInt(Console.readLine());
+                return new DateToVisit(date);
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 식당 예상 방문 날짜는 숫자여야 합니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static OrderList inputOrder() {
+        while(true) {
+            System.out.println("주문하실 메뉴를 메뉴와 개수를 알려주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+            try {
+                List<String> orders = List.of(Console.readLine().split(","));
+                return new OrderList(orders);
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 메뉴의 개수는 숫자여야 합니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
