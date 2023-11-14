36a085b577ac467592be5561dbef188843ed45be
HJLee <goodforx0147@naver.com>
2023-11-14 23:33:04 +0900

    feat: remove space of input date, menu

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
         while(true) {
             System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!)");
             try {
-                int date = Integer.parseInt(Console.readLine());
+                int date = Integer.parseInt(Console.readLine().strip());
                 return new DateToVisit(date);
             } catch (NumberFormatException e) {
                 System.out.println(ExceptionList.DATEEXCPTION.getContent());
@@ -23,7 +23,7 @@ public class InputView {
         while(true) {
             System.out.println("주문하실 메뉴를 메뉴와 개수를 알려주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
             try {
-                List<String> orders = List.of(Console.readLine().split(","));
+                List<String> orders = List.of(Console.readLine().strip().split(","));
                 return new OrderList(orders);
             } catch (NumberFormatException e) {
                 System.out.println(ExceptionList.ORDEREXCEPTION.getContent());
