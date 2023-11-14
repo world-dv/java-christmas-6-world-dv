78b20157d4790f95c9b83ed60334a573db2a1837
HJLee <goodforx0147@naver.com>
2023-11-14 23:53:21 +0900

    test: input menu worng type, duplication test case

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -97,6 +97,23 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 주문_오입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("3", "티본스테이크ㄹ");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 주문_중복_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("3", "티본스테이크-1,티본스테이크-2");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
