c926accfcbc848832ad932388969a8c795e96bf9
HJLee <goodforx0147@naver.com>
2023-11-14 23:44:12 +0900

    test: input date range test case

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -49,6 +49,22 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 날짜_범위_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("32");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 날짜_범위_예외_테스트2() {
+        assertSimpleTest(() -> {
+            runException("0");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
